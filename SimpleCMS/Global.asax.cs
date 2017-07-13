30b4df19bc6a3807c5aafa92673f6da8f6cfd83f
黄灯桥 <tianxiaode@hotmail.com>
2017-07-14 02:19:47 +0800

    第十二章

--- SimpleCMS/Global.asax.cs
+++ SimpleCMS/Global.asax.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleCMS.Helper;
+using SimpleCMS.LocalResources;
 using SimpleCMS.Migrations;
 using SimpleCMS.Models;
 
@@ -18,5 +21,46 @@ namespace SimpleCMS
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            bool isAjaxCall = string.Equals("XMLHttpRequest", Context.Request.Headers["x-requested-with"],
+                StringComparison.OrdinalIgnoreCase);
+            var exception = Server.GetLastError();
+            Response.Clear();
+            var httpStatusCode = (exception as HttpException)?.
+                                 GetHttpCode() ?? 500;
+            string msg = "";
+            switch (httpStatusCode)
+            {
+                case 403:
+                    msg = Message.Forbid403;
+                    break;
+                case 404:
+                    msg = Message.NotFound404;
+                    break;
+                case 500:
+                    msg = Message.InternalServerError500;
+                    break;
+                default:
+                    msg = Message.OtherError;
+                    break;
+            }
+
+            Server.ClearError();
+            if (isAjaxCall)
+            {
+                Response.Write(ExtJs.WriterJObject(false, msg: msg).ToString()); ;
+            }
+            else
+            {
+                Response.Charset = "utf-8";
+                Response.Write(msg);
+            }
+            Response.End();
+
+        }
+
     }
+    
 }
